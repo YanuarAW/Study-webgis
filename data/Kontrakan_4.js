44616364bbeecba5f9f5f61e5fc35d5fcc4581ff
YanuarAW <61992352+YanuarAW@users.noreply.github.com>
2020-03-10 06:31:24 +0700

    Add files via upload

---
+++ data/Kontrakan_4.js
@@ -0,0 +1,11 @@
+var json_Kontrakan_4 = {
+"type": "FeatureCollection",
+"name": "Kontrakan_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Kontrakan Mulawarman Timur No.70", "Jenis": "Kontrakan", "Status": "Kosong", "Alamat": "Mulawarman Timur No 70", "Area": "Mulawarman", "Pemilik": "Eko Purwanto", "CP": "087731534136", "JmlKmr": "5", "Kmr_Mandi": null, "Harga": "30000", "Kamar": null }, "geometry": { "type": "Point", "coordinates": [ 110.441281475000039, -7.068098954999925 ] } },
+{ "type": "Feature", "properties": { "Name": "Kontrakan Timoho Timur 2 No.9", "Jenis": "Kontrakan", "Status": "Kosong", "Alamat": "Timoho Timur 2 No. 9", "Area": "Timoho", "Pemilik": null, "CP": "08995392406", "JmlKmr": "0", "Kmr_Mandi": null, "Harga": "22000", "Kamar": null }, "geometry": { "type": "Point", "coordinates": [ 110.444977503000075, -7.059066890999929 ] } },
+{ "type": "Feature", "properties": { "Name": "Kontrakan Timoho Barat 1 No 10B", "Jenis": "Kontrakan", "Status": "Penuh", "Alamat": "Timoho Barat 1 No. 10B", "Area": "Timoho", "Pemilik": null, "CP": "085803885815", "JmlKmr": "7", "Kmr_Mandi": null, "Harga": "35000", "Kamar": null }, "geometry": { "type": "Point", "coordinates": [ 110.443120519000047, -7.059743092999952 ] } },
+{ "type": "Feature", "properties": { "Name": "Kontrakan Bulusan 11 No.4", "Jenis": "Kontrakan", "Status": "Penuh", "Alamat": "Bulusan 11 No. 4", "Area": "Bulusan", "Pemilik": null, "CP": "085803885815", "JmlKmr": "6", "Kmr_Mandi": null, "Harga": "36000", "Kamar": null }, "geometry": { "type": "Point", "coordinates": [ 110.447789699000054, -7.062236162999966 ] } }
+]
+}
