6b2b91122a03e6ae2b0c376d2c9beacf649372dc
YanuarAW <61992352+YanuarAW@users.noreply.github.com>
2020-03-17 14:29:48 +0700

    Add files via upload

---
+++ data/Kontrakan_3.js
@@ -0,0 +1,15 @@
+var json_Kontrakan_3 = {
+"type": "FeatureCollection",
+"name": "Kontrakan_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Kontrakan Gondang TImur Dalam II (Cika)", "Jenis": "Kontrakan", "Status": "Kosong", "Alamat": "Gondang Timur Dalam II", "Area": "Gondang", "Pemilik": null, "CP": null, "JmlKmr": "3", "Kmr_Mandi": "1", "Harga": "17000" }, "geometry": { "type": "Point", "coordinates": [ 110.444267538416227, -7.062177707435183 ] } },
+{ "type": "Feature", "properties": { "Name": "Kontrakan Gondang Timur Dalam II (Cika 2)", "Jenis": "Kontrakan", "Status": "Kosong", "Alamat": "Gondang Timur Dalam II", "Area": "Gondang", "Pemilik": null, "CP": null, "JmlKmr": "3", "Kmr_Mandi": "1", "Harga": "18500" }, "geometry": { "type": "Point", "coordinates": [ 110.44425837612323, -7.062268151354033 ] } },
+{ "type": "Feature", "properties": { "Name": "Kontrakan Bp. Slamet", "Jenis": "Kontrakan", "Status": "Kosong", "Alamat": "jl. mulawarman 5 no 36 rt 3 rw 1", "Area": "Mulawarman", "Pemilik": "Pak Selamet", "CP": "081215303200", "JmlKmr": "3", "Kmr_Mandi": "2", "Harga": "22000" }, "geometry": { "type": "Point", "coordinates": [ 110.439661102977595, -7.067753280189663 ] } },
+{ "type": "Feature", "properties": { "Name": "Kontrakan Pak Slamet", "Jenis": "Kontrakan", "Status": "Kosong", "Alamat": "jl. mulawarman 5 no 36 rt 3 rw 1", "Area": "Mulawarman", "Pemilik": "Pak Selamet", "CP": "081215303200", "JmlKmr": "10", "Kmr_Mandi": "4", "Harga": "65000" }, "geometry": { "type": "Point", "coordinates": [ 110.439543326863088, -7.067807412182276 ] } },
+{ "type": "Feature", "properties": { "Name": "Kontrakan Mulawarman Timur No.70", "Jenis": "Kontrakan", "Status": "Kosong", "Alamat": "Mulawarman Timur No 70", "Area": "Mulawarman", "Pemilik": "Eko Purwanto", "CP": "087731534136", "JmlKmr": "5", "Kmr_Mandi": null, "Harga": "30000" }, "geometry": { "type": "Point", "coordinates": [ 110.441281475359858, -7.068098955302503 ] } },
+{ "type": "Feature", "properties": { "Name": "Kontrakan Timoho Timur 2 No.9", "Jenis": "Kontrakan", "Status": "Kosong", "Alamat": "Timoho Timur 2 No. 9", "Area": "Timoho", "Pemilik": null, "CP": "08995392406", "JmlKmr": "0", "Kmr_Mandi": null, "Harga": "22000" }, "geometry": { "type": "Point", "coordinates": [ 110.444977502809593, -7.059066891323937 ] } },
+{ "type": "Feature", "properties": { "Name": "Kontrakan Timoho Barat 1 No 10B", "Jenis": "Kontrakan", "Status": "Penuh", "Alamat": "Timoho Barat 1 No. 10B", "Area": "Timoho", "Pemilik": null, "CP": "085803885815", "JmlKmr": "7", "Kmr_Mandi": null, "Harga": "35000" }, "geometry": { "type": "Point", "coordinates": [ 110.443120518796945, -7.059743093369036 ] } },
+{ "type": "Feature", "properties": { "Name": "Kontrakan Bulusan 11 No.4", "Jenis": "Kontrakan", "Status": "Penuh", "Alamat": "Bulusan 11 No. 4", "Area": "Bulusan", "Pemilik": null, "CP": "085803885815", "JmlKmr": "6", "Kmr_Mandi": null, "Harga": "36000" }, "geometry": { "type": "Point", "coordinates": [ 110.447789699035468, -7.062236163368198 ] } }
+]
+}
---
+++ data/Kos_2.js
@@ -0,0 +1,10 @@
+var json_Kos_2 = {
+"type": "FeatureCollection",
+"name": "Kos_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Kost Omah Bimo", "Jenis": "Kos", "Status": "Kosong", "Alamat": "Jl. Bulusan Sel. Dalam III No.34, Bulusan", "Area": "Bulusan", "Pemilik": "Bu Hesti", "CP": "081214466802", "JmlKmr": "20", "Kmr_Mandi": "Dalam", "Harga": "550\/800\/1200", "Kamar": "Premium\/Semi Eksklusif\/Eksklusif" }, "geometry": { "type": "Point", "coordinates": [ 110.447553776000063, -7.062661022999976 ] } },
+{ "type": "Feature", "properties": { "Name": "Wisma Ananda Kos Putra", "Jenis": "Kos", "Status": "Kosong", "Alamat": "Jl. Gondang Barat III, Bulusan", "Area": "Gondang", "Pemilik": null, "CP": null, "JmlKmr": "0", "Kmr_Mandi": "Dalam\/Luar", "Harga": "550\/750\/700", "Kamar": "Biasa, AC, Km.Dalam" }, "geometry": { "type": "Point", "coordinates": [ 110.439982589000067, -7.063954548999959 ] } },
+{ "type": "Feature", "properties": { "Name": "Kost Putri Ekslusif", "Jenis": "Kos", "Status": "Kosong", "Alamat": "Jl. Tlogosari No.raya", "Area": "Tlogosari", "Pemilik": "Pak Soepomo", "CP": "085225423354", "JmlKmr": "44", "Kmr_Mandi": "Dalam", "Harga": "1000\/950\/900", "Kamar": "Ekslusif Lt.1,2,3" }, "geometry": { "type": "Point", "coordinates": [ 110.43743991100007, -7.062576570999965 ] } }
+]
+}
